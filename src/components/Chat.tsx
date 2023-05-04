041dd5f7301880d05384d7d6a8a752826fadbab4
rojassrodrigo <rojasrodrigo@live.com>
2023-05-04 20:48:13 -0300

    20 commit

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -110,38 +110,50 @@ const handleSubmit = async (event: React.FormEvent) => {
               Abri Chat
           </button>
         ) : (
-            <div>
-                      <button className='text-xl rounded-full bg-blue-800 w-8 h-8 absolute -right-4 -top-5' onClick={() => toggleCollapsed(true)}>x</button>
-                     
-                        <div className='flex flex-col gap-4 m-auto max-w-lg border border-gray-400 p-4 rounded-md'>
-                          <div ref={ref} className='flex flex-col gap-4 h-[250px] overflow-y-auto'>
-                          {
-                            messages.map((message) => (
-                              <div 
-                                key={message.id} 
-                                className={`p-4 max-w-[80%] rounded-3xl text-white ${
-                                message.type === "bot" 
-                                ? "bg-slate-500 text-left self-start rounded-bl-none" 
-                                : "bg-blue-500 text-right self-end rounded-br-none"}`}
-                                >
-                                  {message.text}
-                                  </div>
-                            ))
-                          }
-                          </div>
-                          <form className='flex items-center' onSubmit={handleSubmit}>
-                            <input 
-                            value={question}
-                            onChange={(event) => setQuestion(event.target.value)}
-                            className='rounded rounded-r-none flex-1 border border-gray-400 py-2 px-4' 
-                            type='text'
-                            placeholder='Escribe tu pregunta...'
-                            style={{ color: 'black' }} // Agregar esta línea
-                            />
-                            <button className={`px-4 py-2 bg-blue-500 rounded-lg rounded-l-none ${isLoading ? "bg-blue-300" : "bg-blue-500" }`} type='submit'>Enviar</button>
-                          </form>
-                        </div>
-            </div>
+<div>
+  <button
+    className='text-xl rounded-full bg-blue-800 w-8 h-8 absolute -right-4 -top-5'
+    onClick={() => toggleCollapsed(true)}
+  >
+    x
+  </button>
+
+  <div className='flex flex-col gap-4 mx-4 md:mx-auto md:max-w-lg border border-gray-400 p-4 rounded-md'>
+    <div ref={ref} className='flex flex-col gap-4 h-[250px] overflow-y-auto'>
+      {messages.map((message) => (
+        <div
+          key={message.id}
+          className={`p-4 max-w-[80%] rounded-3xl text-white ${
+            message.type === 'bot'
+              ? 'bg-slate-500 text-left self-start rounded-bl-none'
+              : 'bg-blue-500 text-right self-end rounded-br-none'
+          }`}
+        >
+          {message.text}
+        </div>
+      ))}
+    </div>
+    <form className='flex items-center' onSubmit={handleSubmit}>
+      <input
+        value={question}
+        onChange={(event) => setQuestion(event.target.value)}
+        className='rounded rounded-r-none flex-1 border border-gray-400 py-2 px-4'
+        type='text'
+        placeholder='Escribe tu pregunta...'
+        style={{ color: 'black' }}
+      />
+      <button
+        className={`px-4 py-2 bg-blue-500 rounded-lg rounded-l-none ${
+          isLoading ? 'bg-blue-300' : 'bg-blue-500'
+        }`}
+        type='submit'
+      >
+        Enviar
+      </button>
+    </form>
+  </div>
+</div>
+
         )
       }
     </div>
